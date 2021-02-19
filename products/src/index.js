8329ac12f7beb9c89d82746317114ab2c05bae81
“dbp” <byronpantoja@gmail.com>
2021-02-19 10:44:58 +0800

    initializing webpack

--- products/src/index.js
+++ products/src/index.js
@@ -3,7 +3,7 @@ import faker from 'faker';
 let products = '';
 
 for (let i = 0; i < 3; i++) {
-  const name = faker.commerce,productName();
+  const name = faker.commerce.productName();
   products += `<div>${name}</div>`;
 }
 
